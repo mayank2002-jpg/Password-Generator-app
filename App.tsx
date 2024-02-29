355f3c10303ace9b00b66bcc49f76f4898f6f923
Mayank <mayankmahajan936@gmail.com>
2024-02-29 10:51:53 +0530

    function implemented

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
 //Form Validation
 import * as Yup from 'yup';
 
@@ -11,6 +11,49 @@ const PasswordSchema = Yup.object().shape({
 });
 
 export default function App() {
+  const [password, setPassword] = useState('');
+  const [isPassGenerated, setIsPassGenerated] = useState(false);
+  const [lowerCase, setLowerCase] = useState(true);
+  const [upperCase, setUpperCase] = useState(false);
+  const [numbers, setNumbers] = useState(false);
+  const [symbols, setSymbols] = useState(false);
+
+  const generatePasswordString = (passwordLength: number) => {
+    let characterList = '';
+
+    const upperCaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const lowerCaseChars = 'abcdefghijklmnopqrstuvwxyz';
+    const digitChars = '0123456789';
+    const specialChars = '!@#$%^&*()_+';
+
+    if (lowerCase) characterList += lowerCaseChars;
+    if (upperCase) characterList += upperCaseChars;
+    if (numbers) characterList += digitChars;
+    if (symbols) characterList += specialChars;
+
+    const passwordResult = createPassword(characterList, passwordLength);
+    setPassword(passwordResult);
+    setIsPassGenerated(true);
+  };
+
+  const createPassword = (characters: string, passwordLength: number) => {
+    let result = '';
+    for (let i = 0; i < passwordLength; i++) {
+      const charaterIndex = Math.round(Math.random() * characters.length);
+      result += characters.charAt(charaterIndex);
+    }
+    return result;
+  };
+
+  const resetPasswordState = () => {
+    setPassword('');
+    setIsPassGenerated(false);
+    setLowerCase(true);
+    setUpperCase(false);
+    setNumbers(false);
+    setSymbols(false);
+  };
+
   return (
     <View>
       <Text>App</Text>
